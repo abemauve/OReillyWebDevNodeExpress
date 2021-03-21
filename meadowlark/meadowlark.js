c35424b1b5c1828298e9712badb60ea28bc0ea1e
Abe Mauve <abemauve@gmail.com>
2021-03-21 08:59:47 +0500

    Added main route in meadowlark

--- meadowlark/meadowlark.js
+++ meadowlark/meadowlark.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+// main route
+app.get('/', (req, res) => {
+    res.type('text/plain')
+    res.send('Meadowlark Travel')
+})
+
 // custom 404 page
 app.use((req, res) => {
     res.type('text/plain')
