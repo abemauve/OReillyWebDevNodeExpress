a536a01cfdff4a58bbdbc0c44f7d50b70d9be1fa
Abe Mauve <abemauve@gmail.com>
2021-03-22 23:37:10 +0500

    Misc changes

--- meadowlark/meadowlark.js
+++ meadowlark/meadowlark.js
@@ -2,7 +2,6 @@ const express = require('express')
 const expressHandlebars = require('express-handlebars')
 
 const handlers = require('./lib/handlers')
-const fortune = require('./lib/fortune')
 
 const app = express()
 
