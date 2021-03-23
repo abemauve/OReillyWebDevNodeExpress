b12eebb3a186f79d8272c21e164c1d87ea49d51a
Abe Mauve <abemauve@gmail.com>
2021-03-24 00:46:42 +0500

    Added newsletter signup via normal form POST submission and via Fetch API based POST data submission

--- meadowlark/lib/handlers.js
+++ meadowlark/lib/handlers.js
@@ -9,6 +9,42 @@ exports.about = (req, res) =>
 
 exports.apiFortunes = (req, res) => res.json(fortune.getAllFortunes())
 
+//-----------------------------------------------------------------------------
+
+exports.newsletterSignup = (req, res) => {
+  // for now we just provide a dummy value for csrf token
+  res.render('newsletter-signup', { csrf: 'CSRF token goes here' })
+}
+
+exports.newsletterSignupProcess = (req, res) => {
+  console.log('Form (from querystring): ' + req.query.form)
+  console.log('CSRF token (from hidden form field): ' + req.body._csrf)
+  console.log('Name (from visible form field): ' + req.body.name)
+  console.log('E-mail (from visible form): ' + req.body.email)
+  res.redirect(303, '/newsletter-signup/thank-you')
+}
+
+exports.newsletterSignupThankYou = (req, res) => 
+  res.render('newsletter-signup-thank-you')
+
+//-----------------------------------------------------------------------------
+
+exports.newsletterSignupViaFetchAPI = (req, res) => {
+  // for now we just provide a dummy value for csrf token
+  res.render('newsletter-fetchapi-signup', { csrf: 'CSRF token goes here' })
+}
+
+exports.api = {
+  newsletterSignup: (req, res) => {
+    console.log('CSRF token (from hidden form field): ' + req.body._csrf)
+    console.log('Name (from visible form field): ' + req.body.name)
+    console.log('Email (from visible form field): ' + req.body.email)
+    res.send({ result: 'success' })
+  },
+}
+
+//-----------------------------------------------------------------------------
+
 exports.notFound = (req, res) => res.render('404')
 
 /* eslint-disable no-unused-vars */
--- meadowlark/meadowlark.js
+++ meadowlark/meadowlark.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 const handlers = require('./lib/handlers')
+const weatherMiddleware = require('./lib/middleware/weather')
 
 const app = express()
 app.disable('x-powered-by')
@@ -24,6 +26,10 @@ app.set('view engine', 'handlebars')
 const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'))
+app.use(weatherMiddleware)
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 // main route
 app.get('/', handlers.home)
@@ -40,6 +46,17 @@ app.get('/headers', (req, res) => {
 })
 
 app.get('/api/fortunes', handlers.apiFortunes)
+
+
+// normal signup
+app.get('/newsletter-signup', handlers.newsletterSignup)
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
+
+// Fetch API signup
+app.get('/newsletter-fetchapi-signup', handlers.newsletterSignupViaFetchAPI)
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
+
 
 // custom 404 page
 app.use(handlers.notFound)
